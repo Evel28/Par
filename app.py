cf94d7a46c432f3fe27800f5171d275bb7a36fce
Evel <evenezuela28@gmail.com>
2018-10-14 12:29:00 -0430

    commit otro

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-print("Estamos aprendiendo Python")
-print("Estamos aprendiendo instrucciones básicas")
-print("Poco a poco iremos aprendiendo")
+def mensaje():
+    
+	print("Estamos aprendiendo Python")
+	print("Estamos aprendiendo instrucciones básicas")
+	print("Poco a poco iremos avanzando")
+
+mensaje()
